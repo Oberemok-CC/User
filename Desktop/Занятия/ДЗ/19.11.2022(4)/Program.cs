d8d443702dd9c46f13e508bd5f277d7aa5ef5e01
Oberemok-CC <boserf_serg@mail.ru>
2022-11-21 22:28:15 +0300

    добавлено домашнее задание от 21.11.2022

---
+++ Desktop/Занятия/ДЗ/19.11.2022(4)/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая на вход принимает число (N), 
+// а на выходе показывает все чётные числа от 1 до N.
+
+
+Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int num = 2;
+    if (N %2 == 0)  
+     { while (num < N)
+       { 
+       Console.Write(num + ", "); 
+       num = num +2; 
+       }
+
+    Console.Write(N + ". ");
+     }
+    else
+    {  while (num < N-1)
+        {
+        Console.Write(num + ", "); 
+        num = num +2; 
+        }
+        Console.Write(N-1 + ". ");
+    }
+    
+
