b2b56c0a3d04c4061953197527b9e1d9da4a99c1
Oberemok-CC <boserf_serg@mail.ru>
2022-11-26 00:15:34 +0300

    выполнено ДЗ по четвертому семинару

---
+++ Desktop/Занятия/ДЗ/25.11.2022_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] array = new int[8];
+int [] GetArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i]= new Random().Next(0,9);
+    }  
+    return array;
+}
+GetArray(array);
+Console.WriteLine($"[{String.Join(" ",array)}]");
