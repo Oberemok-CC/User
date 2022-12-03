1245e54167ac11afc17a2eb1fd761d4e47c5fc69
Oberemok-CC <boserf_serg@mail.ru>
2022-12-03 19:28:03 +0300

    добавлена ДЗ по девятому семинару

---
+++ Desktop/Занятия/ДЗ/04.12.2022_2/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму 
+// натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+// Сделал сумму промежутка не включая крайние M и N
+
+Console.Write("Введите число M:  ");
+int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число N:  ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+
+int start = M+1;
+int end = N-1;
+int Sum(int start, int end)
+
+{
+    if (start==end) return start;
+    return(start + Sum(start+1, end));
+}
+Console.WriteLine(Sum(start, end));
+
---
+++ Desktop/Занятия/ДЗ/04.12.2022_3/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и m.
+// m = 2, m = 3 -> A(m,m) = 9
+// m = 3, m = 2 -> A(m,m) = 29
+
+// ![](https://www.bestprog.net/wp-content/uploads/2019/01/recursion_05r.jpg);
+
+Console.Write("Введите число m:  ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число n:  ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+int A(int m, int n)
+{
+  if (m == 0) return n + 1;
+  else
+    if ((m != 0) && (n == 0)) return A(m - 1, 1);
+    else
+    return A(m - 1, A(m, n - 1));
+}
+Console.Write(A(m, n));
